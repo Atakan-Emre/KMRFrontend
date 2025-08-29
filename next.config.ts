264cd4e9343f9b578054575071ec7d92c313f518
Atakan Emre <satakanemre@gmail.com>
2025-08-29 22:55:30 +0300

    Next.js yapılandırması güncellendi, `dev` komutu için `--turbopack` eklendi. `deploy.yml`, `middleware.ts` ve `login/page.tsx` dosyaları silindi.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-import { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/KMRProject' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/KMRProject/' : '',
+  /* config options here */
 };
 
 export default nextConfig;
