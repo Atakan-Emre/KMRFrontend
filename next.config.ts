25c58369512a58e1277f48b7e101cdb734e3185e
Atakan Emre <satakanemre@gmail.com>
2025-08-29 23:06:00 +0300

    GitHub Pages yapılandırması eklendi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   basePath: process.env.NODE_ENV === 'production' ? '/KMRFrontend' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/KMRFrontend/' : '',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
