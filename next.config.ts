d9395368a43aed0c3eb51f5c1e38513c3842562a
Atakan Emre <satakanemre@gmail.com>
2025-08-29 22:27:02 +0300

    first commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/KMRProject' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/KMRProject/' : '',
+};
+
+export default nextConfig;
